65ff4d65c83c882c86caac8d34ce9a4c42c9ff1e
Skorp 1.0 <skorp1gaming@web.de>
2024-05-26 20:09:08 +0200

    added .nickremove

---
+++ Commands/nickremove.cs
@@ -0,0 +1,46 @@
+﻿using CommandSystem;
+using System;
+using Exiled.API.Features;
+using PlayerRoles;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace NGMainPlugin.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class NickRemove : ParentCommand
+    {
+        public static List<string> spoke = new List<string>();
+
+        public static Main Plugin { get; set; }
+
+        public NickRemove()
+        {
+            LoadGeneratedCommands();
+        }
+
+        public override string Command { get; } = "nickremove";
+
+        public override string[] Aliases { get; } = new string[] { };
+
+        public override string Description { get; } = "Remove your nickname.";
+
+        public override void LoadGeneratedCommands() { }
+
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+
+            if (player == null)
+            {
+                response = "Player is null, contact an admin/developer!";
+                return false;
+            }
+
+            player.DisplayNickname = null;
+
+            response = "Your nick has been removed!";
+            return true;
+        }
+    }
+}
