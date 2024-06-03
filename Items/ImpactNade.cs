1a4c84e483ad90d2e873edbb0c4c9d771112badd
Skorp 1.0 <skorp1gaming@web.de>
2024-06-03 21:39:32 +0200

    Only added the file lol

---
+++ Items/ImpactNade.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Features.Attributes;
+using Exiled.CustomItems.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGMainPlugin.Items
+{
+    [CustomItem(ItemType.GrenadeHE)]
+    internal class ImpactNade : CustomGrenade
+    {
+    }
+}
