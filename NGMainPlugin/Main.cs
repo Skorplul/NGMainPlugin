daaadd484942bc0dcfde72334c427b3c7406de70
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-07-24 23:03:57 +0200

    incremented version to 1.4.0

--- NGMainPlugin/Main.cs
+++ NGMainPlugin/Main.cs
@@ -23,7 +23,7 @@ namespace NGMainPlugin
         public override string Author { get; } = "Skorp 1.0 and LastPenguin";
         public override string Name { get; } = "NGMainPlugin (Dev-Build)";
         public override string Prefix { get; } = "NGM (Dev)";
-        public override Version Version { get; } = new Version(1, 3, 3);
+        public override Version Version { get; } = new Version(1, 4, 0);
 
         public override Version RequiredExiledVersion { get; } = new Version(8, 9, 11);
 
